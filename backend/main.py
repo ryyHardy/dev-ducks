225c9bddb3e38ae47daba83300b795bc3ca21109
ryyHardy <ryyHardy@gmail.com>
2025-07-06 19:44:53 -0400

    fastapi now handles request as a single object and not url params

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,36 @@
 import os
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 from llm import TwitchGeneratorClient
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Restrict this to specific domains in production
+    allow_origins=["*"],  # Restrict this to specific domains in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-PORT = int(os.getenv("PORT", 8000)) # Default to 8000 if not set
+PORT = int(os.getenv("PORT", 8000))  # Default to 8000 if not set
 
 client = TwitchGeneratorClient()
 
 
+class GenerateChatRequest(BaseModel):
+    code: str
+    count: int
+
+
 @app.post("/generate_chat")
-def generate_chat(code: str, count: int):
-    messages = client.generate_chat(code, count)
+def generate_chat(request: GenerateChatRequest):
+    messages = client.generate_chat(request.code, request.count)
     return messages
+
 
 # @app.post("/donation_message")
 # def donation_message(input_dat: str):
